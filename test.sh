f696295457d26e1fed316482b65e750693988839
Ajit George <gajit@nightskate.local>
2024-12-09 10:40:19 -0700

    Add support for AlmaLinux-based iamges
    AlmaLinux is a clone of RHEL.

--- test.sh
+++ test.sh
@@ -43,7 +43,7 @@ test_suite() {
     docker compose stop -t 1 test-$image_name
 }
 
-images=${*:-"alpine ubuntu ubuntu-14.04 debian amazonlinux rockylinux8 rockylinux9 fedora"}
+images=${*:-"alpine ubuntu ubuntu-14.04 debian amazonlinux rockylinux8 rockylinux9 fedora almalinux"}
 
 for image in $images; do
     test_suite $image
